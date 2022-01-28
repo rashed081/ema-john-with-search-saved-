60f66f239ddb0b017659cab52a1c1f99a2168c78
Rashed Rony <rashed.khan.rony.me@gmail.com>
2022-01-28 22:40:11 +0600

    inventory & review page updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Shop from "./components/Shop/Shop";
+import OrderReview from "./components/Order Review/OrderReview";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="order-review" element={<OrderReview />} />
+        <Route path="inventory" element={<Inventory />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
