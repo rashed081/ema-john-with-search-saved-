c7d36ed46d4f3e4c60c6bd7409d421a180818167
Rashed Rony <rashed.khan.rony.me@gmail.com>
2022-01-29 22:53:45 +0600

    add to cart bug fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -37,7 +37,17 @@ const Shop = () => {
   }, [products]);
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+    let newCart = [];
+    const exist = cart.find((pd) => pd.key === product.key);
+    if (exist) {
+      const rest = cart.filter((pd) => pd.key !== product.key);
+      exist.quantity = exist.quantity + 1;
+      newCart = [...rest, product];
+    } else {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }
+
     setCart(newCart);
     // save to local storage (for now)
     addToDb(product.key);
